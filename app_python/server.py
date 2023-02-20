4498ba92af12aeac6a52843575ba7ce382650ae5
Alexey <a.smolyakov@tochka.com>
2023-02-20 05:11:21 +0300

    Lab 8 logging

--- app_python/server.py
+++ app_python/server.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from datetime import datetime
 from pytz import timezone
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def time():
